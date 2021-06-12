ade9237bafd15a06b19556f03fd900d454401afc
VeresRoland <veres.roland1@gmail.com>
2021-06-12 21:34:13 +0200

    Kész3

--- frontend/src/app/munkasok/show-mun/show-mun.component.ts
+++ frontend/src/app/munkasok/show-mun/show-mun.component.ts
@@ -20,6 +20,13 @@ export class ShowMunComponent implements OnInit {
   ActivateAddEditMunComp:boolean=false;
   mun:any;
 
+  MunkasIDFilter:string="";
+  MunkasNeveFilter:string="";
+  MunkasSzakkepzettsegFilter:string="";
+  MunkasOraberFilter:string="";
+  MunkaNeveFilter:string="";
+  MunkasokListWithoutFilter:any=[];
+
   ngOnInit(): void {
     this.refreshMunList();
 
@@ -40,7 +47,7 @@ export class ShowMunComponent implements OnInit {
   }
 
   editClick(item){
-    console.log(item);
+    //console.log(item);
     this.mun=item;
     this.ModalTitle="Munkás adatainak módosítása";
     this.ActivateAddEditMunComp=true;
@@ -63,8 +70,44 @@ export class ShowMunComponent implements OnInit {
   refreshMunList(){
     this.service.getMunList().subscribe(data=>{
       this.MunkasokList=data;
+      this.MunkasokListWithoutFilter=data;
+    });
+  }
+
+  FilterFn(){
+    var MunkasIDFilter = this.MunkasIDFilter;
+    var MunkasNeveFilter = this.MunkasNeveFilter;
+    var MunkasSzakkepzettsegFilter = this.MunkasSzakkepzettsegFilter;
+    var MunkasOraberFilter = this.MunkasOraberFilter;
+    var MunkaNeveFilter = this.MunkaNeveFilter;
+
+    this.MunkasokList = this.MunkasokListWithoutFilter.filter(function (el){
+      return el.MunkasID.toString().toLowerCase().includes(
+        MunkasIDFilter.toString().trim().toLowerCase()
+      )&&
+        el.MunkasNeve.toString().toLowerCase().includes(
+        MunkasNeveFilter.toString().trim().toLowerCase()
+      )&&
+        el.MunkasSzakkepzettseg.toString().toLowerCase().includes(
+        MunkasSzakkepzettsegFilter.toString().trim().toLowerCase()
+      )&&   
+        el.MunkasOraber.toString().toLowerCase().includes(
+        MunkasOraberFilter.toString().trim().toLowerCase()
+      )&&
+        el.MunkaNeve.toString().toLowerCase().includes(
+        MunkaNeveFilter.toString().trim().toLowerCase()
+    )
 
     });
+  }
+    sortResult(prop,asc){
+      this.MunkasokList = this.MunkasokListWithoutFilter.sort(function(a,b){
+        if(asc){
+            return (a[prop]>b[prop])?1 : ((a[prop]<b[prop]) ?-1 :0);
+        }else{
+          return (b[prop]>a[prop])?1 : ((b[prop]<a[prop]) ?-1 :0);
+        }
+      })
   }
 
 }
